9750b028cc62def63c88f888b6d9f02c27b4e799
diego <diegozaratean@gmail.com>
2025-05-07 17:43:20 +0000

    finish js intro clas

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('hola desde main.js')
+console.log('1')
+console.log('2')
+console.log('3')
+
+
+// elemento.     1.         2.      3                4           5       
+let frutas = ['mamzana','sandia','palta/aguacate','mango','mandarina']
+// positiobn.    0          1          2            3           4
+
+
+let carro = { marca: 'tesla',color:"rojo", precio: 30000 , disponible: true}
