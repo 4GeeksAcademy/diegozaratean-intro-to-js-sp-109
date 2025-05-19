04212700adc8d118242dfbc9b74ca99080a2aa10
diego <diegozaratean@gmail.com>
2025-05-19 17:44:48 +0000

    finish conditional profiel card class

--- main.js
+++ main.js
@@ -1,121 +1,39 @@
 console.log('hola desde main.js')
 
-// Array
-// Mario
-// Luigi
-// Bowser
-// Toad
-// Peach
+// desarrollar un programa que calcule el impuesto que debo pagar dependiento de mi salario
+// si el salario es mayor o igual a 1000 imprimir debes pagar un 10% si no imprimir debes pagar un 5%
 
-let personajesMario = ['Mario','Luigi','Bowser','Toad','Peach', 'star']
+// PSEUDOCODIGO
+// definir variable salario y asignarle valor
+let salario = 5000
+// comparar el salario mayor o igual a mil
+//si salario igual o mayor a mil  
+// let mensaje = ''
+// if (salario >= 1000  ){
+//     // imprimir debes pagar 10%
+//      mensaje ='debes pagar 10%'
 
-console.log(personajesMario)
+// }else{
+//     // imprimir debes pagar 5%
+//      mensaje ='debes pagar 5%'
+// }
+// ternary operator
+// if(condicion){
+    //     que hago true
+    // }elser{
+        //     que hago si false
+        // }
+// (condicion) ? que hago true : que hago si false
+let mensaje = salario >= 1000   ? 'debes pagar 10%' :'debes pagar 5%'
+// document.getElementById("resultado").innerText = mensaje
 
-console.log(personajesMario[0])
-console.log(personajesMario[1])
-console.log(personajesMario[2])
-console.log(personajesMario[3])
-console.log(personajesMario[4])
-
-// for(inicializacion;condicion;actualizacion){}
-for(let i =0; i < personajesMario.length; i++){
-    console.log(personajesMario[i]);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-// let numbers = [1,2,3,4]
-console.log(numbers)
-
-// i = i + 3 ======> i+=3
-// i = i + 1 ======> i++
-
-
-// i = 0 =====> 1
-// i = 1 =====> 2
-// i = 2 =====> 3
-// i = 3 =====> 4
-// i = 4 =====> numbers[4] no existe  undefinded
-
-
-// i = 0 =====> 1
-// i = 2 =====> 3
-// i = 4 =====> numbers[4] no existe  undefinded
-
-
-// i = 0 =====> 1
-// i = 3 =====> 4
-// i = 6 =====> 
-for(i = 0;i <= numbers.length; i+=3){
-    // console.log(i)
-    console.log(numbers[i])
-}
-
-
-// crear un nuevo array con los personajes buenos
-let goodGamesNames = []
-for(i=0; i < personajesMario.length;i++){
-    if(personajesMario[i] != 'Bowser'){
-        goodGamesNames.push(personajesMario[i])
-    }
-}
-
-console.log(personajesMario)
-console.log(goodGamesNames)
-
-console.log(personajesMario.filter( (personaje)=> personaje != 'Bowser' ))
-
-console.log(numbers)
-console.log(numbers.filter( (numero)=> numero != 7 ))
-console.log(numbers.filter( (numero)=> numero > 7 ))
-console.log(numbers.filter( (numero)=> numero <= 15 ))
-console.log(numbers.filter( (numero)=> numero > 7 && numero <= 15 ))
-
-
-// impirmir un array de los number multiplciados por 4 
-let newNumbers = []
-// 1,2,3,4 etc 4,8,12,16
-for(i = 0 ; i < numbers.length; i++){
-    console.log(numbers[i]*4)
-    newNumbers.push(numbers[i]*4)
-
-}
-
-console.log(numbers)
-console.log(newNumbers)
-
-let mapNumbers = numbers.map( (number)=> number * 4 )
-console.log(mapNumbers)
-
-console.log(personajesMario)
-console.log(personajesMario.map( (personaje)=> personaje + 's' ))
-
-
-personajesMario.forEach( (item)=>{
-    console.log(item)
-})
-
-console.log('Do while')
-let j = 20
-do{
-    console.log(j)
-    j++
-}while( j < 8)
+document.getElementById("resultado").insertAdjacentHTML(
+    "afterend",
+    `<div><h1> hola este es el impuesto</h1><p> mensaje $ ${mensaje}</p></div>`
+)
+        
+        
+        
+console.log(mensaje)
+console.log('p> mensaje $ ${mensaje}</p>')
+console.log(`p> mensaje $ ${mensaje}</p>`)
